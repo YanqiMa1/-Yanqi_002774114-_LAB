f762b6195d7dc7312217155627dcf34006bbc3c9
YanqiMa1 <ma.yanqi1@northeastern.edu>
2023-01-29 20:48:50 -0500

    upload labwork2

---
+++ LABTwo/src/Model/Customer.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Ma2017
+ */
+public class Customer {
+    String customerName;
+    int customerId;
+    
+    public Customer(){
+        
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    
+  
+    
+    
+    
+}
