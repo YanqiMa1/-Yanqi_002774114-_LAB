89d5563783a44f5e9e0db79f9841ebf417078dac
YanqiMa1 <ma.yanqi1@northeastern.edu>
2023-02-05 16:31:51 -0500

    first upload

---
+++ LabExercise3_yanqi/src/Model/Medicine.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Ma2017
+ */
+public class Medicine {
+    
+    String medicineName;
+    double dosage;
+    
+    public Medicine(){
+        
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public double getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(double dosage) {
+        this.dosage = dosage;
+    }
+
+    @Override
+    public String toString(){
+        return this.medicineName;
+    }
+    
+}
